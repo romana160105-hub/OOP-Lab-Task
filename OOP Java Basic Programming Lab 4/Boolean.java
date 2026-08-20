e78977a4e96e18312523778f876f88f39a8471ca
romana160105-hub <romana160105@gmail.com>
2026-08-20 14:03:46 +0600

    Create Boolean.java

---
+++ OOP Java Basic Programming Lab 4/Boolean.java
@@ -0,0 +1,9 @@
+public class Boolean {
+  public static void main(String[] args) {
+    boolean isRaining = true;
+
+    if (isRaining) {
+      System.out.println("Bring an umbrella!");
+    }
+  }
+}
